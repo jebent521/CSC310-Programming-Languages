7256ee311a426bb86a1c3674be6f9089f127bb8e
Jonah Ebent <jonahaaronebent@gmail.com>
2023-10-16 16:39:50 -0400

    fixed turn, made hilbert

--- Python/LittleQuilt.py
+++ Python/LittleQuilt.py
@@ -12,27 +12,35 @@ An oriented atom is a tuple of the form ("q",i) where "q" is a quilt atom and i 
     ("q", i) represents "q" rotated clockwise through i clockwise 90 degree rotations.
 A quilt atom will be a single letter either "a" or "b".
 """
-from itertools import product
-import typing
 
 def main():
-    myquilt=turn(sew(a, b))
+    myquilt=hilbert(hilbert(hilbert(a)))
     for row in myquilt:
         print(row)
 
 a = [[('a', 0)]]
 b = [[('b', 0)]]
 
-def turn(quilt):
-    newQuilt=[[None] * len(quilt)] * len(quilt[0])
-    for i, j in product(range(len(quilt)), range(len(quilt[0]))):
-        atom = (quilt[i][j])[0]
-        orientation = ((quilt[i][j])[1] + 1) % 4
-        newQuilt[j][i] = (atom, orientation)
+def turn_atom(atom):
+    return (atom[0], (atom[1] + 1) % 4)
+
+def metamap(f, quilt):
+    return [list(map(f, row)) for row in quilt]
+
+def rotate(quilt):
+    newQuilt = []
+    maxC = len(quilt) - 1
+    for r in range(len(quilt[0])):
+        newRow = []
+        for c in range(len(quilt)):
+            newRow.append(quilt[maxC - c][r])
+        newQuilt.append(newRow)
     return newQuilt
 
+def turn(quilt):
+    return metamap(turn_atom, rotate(quilt))
 
-def sew(quilt1, quilt2):
+def sew(quilt1, quilt2):        # sews quilt2 to the right of quilt1
     newQuilt = []
     for i,j in zip(quilt1, quilt2):
         newQuilt.append(i + j)
@@ -42,7 +50,7 @@ def unturn(quilt):              # turns quilt once counterclockwise
     return turn(turn(turn(quilt)))
 
 def pile(quilt1, quilt2):       # piles quilt1 on top of quilt2
-    return unturn(sew(turn(quilt2), turn(quilt1)))
+    return unturn(sew(turn(quilt1), turn(quilt2)))
 
 def pinwheel(quilt):
     pass
@@ -53,4 +61,6 @@ def repeat_block(quilt, m, n):
 def quilt_to_string(quilt, string1, string2):
     pass
 
+def hilbert(quilt):
+    return sew(pile(turn(quilt), quilt), pile(unturn(quilt), quilt))
 main()
