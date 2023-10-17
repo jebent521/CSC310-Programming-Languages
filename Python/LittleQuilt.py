2da6aa1809961c8162749afcc0d9b04f92fd4da9
Jonah Ebent <jonahaaronebent@gmail.com>
2023-10-17 18:37:43 -0400

    quilt_to_string method, edited hilbert

--- Python/LittleQuilt.py
+++ Python/LittleQuilt.py
@@ -14,13 +14,20 @@ A quilt atom will be a single letter either "a" or "b".
 """
 # Main Method
 def main():
-    myquilt=pseudo_hilbert(3, a)
+    #myquilt=pseudo_hilbert(3, a)
+    #for row in myquilt:
+    #    print(row)
+    myquilt = pile(sew(a, turn(b)), sew(unturn(b), turn(turn(a))))
     for row in myquilt:
         print(row)
+    
+    #print(quilt_to_string(myquilt, a_pattern, b_pattern))
 
 # Defining Quilt Atoms
 a = [[('a', 0)]]
 b = [[('b', 0)]]
+a_pattern = ['┌─\n│┌', '─┐\n┐│', '┘│\n─┘', '│└\n└─']
+b_pattern = ['╭─\n│╭', '─╮\n╮│', '╯│\n─╯', '│╰\n╰─']
 
 # Turn Function (uses turn_atom, metamap, and rotate)
 def turn_atom(atom):
@@ -42,18 +49,30 @@ def rotate(quilt):
 def turn(quilt):
     return metamap(turn_atom, rotate(quilt))
 
-# Sew Function
-def sew(quilt1, quilt2):        # sews quilt2 to the right of quilt1
+# Sew Function: sews quilt2 to the right of quilt1
+def sew(quilt1, quilt2):
     newQuilt = []
     for i,j in zip(quilt1, quilt2):
         newQuilt.append(i + j)
     return newQuilt
 
+# Quilt To String Function: returns a stringified quilt
+def quilt_to_string(quilt, string1, string2):
+    result = ''
+    for row in quilt:
+        for atom in row:
+            if atom[0] == 'a':
+                result += string1[atom[1]]
+            else:
+                result += string2[atom[1]]
+        result += '\n'
+    return result
+
 # Extra (Convenience) Functions
 def unturn(quilt):              # turns quilt once counterclockwise
     return turn(turn(turn(quilt)))
 
-def pile(quilt1, quilt2):       # piles quilt1 on top of quilt2
+def pile(quilt1, quilt2):       # piles quilt2 on top of quilt1
     return unturn(sew(turn(quilt1), turn(quilt2)))
 
 def pinwheel(quilt):
@@ -62,15 +81,9 @@ def pinwheel(quilt):
 def repeat_block(quilt, m, n):
     pass
 
-def quilt_to_string(quilt, string1, string2):
-    pass
-
-def hilbert_pattern(quilt):
-    return sew(pile(turn(quilt), quilt), pile(unturn(quilt), quilt))
-
 def pseudo_hilbert(n, quilt):
     for i in range(n):
-        quilt = hilbert_pattern(quilt)
+        quilt = sew(pile(turn(quilt), quilt), pile(unturn(quilt), quilt))
     return quilt
 
 main()
