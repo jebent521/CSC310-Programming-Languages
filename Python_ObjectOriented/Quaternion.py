a40ae0646ec750dd0a8b2d04ef15b135a2053650
Jonah Ebent <jonahaaronebent@gmail.com>
2023-11-13 23:12:21 -0500

    wrote function headers

--- Python_ObjectOriented/Quaternion.py
+++ Python_ObjectOriented/Quaternion.py
@@ -1,10 +1,45 @@
 class Quaternion:
     def __init__(self, a, b, c, d) -> None:
         pass
+
     def __repr__(self) -> str:
         pass
+    
     def __str__(self) -> str:
         pass
+    
+    def __abs__(self):
+        pass
+    
+    def __add__(self):
+        pass
+    
+    def __iadd__(self):
+        pass
+    
+    def __sub__(self):
+        pass
+    
+    def __isub__(self):
+        pass
+    
+    def __mul__(self):
+        pass
+    
+    def __imul__(self):
+        pass
+    
+    def __rmul__(self):
+        pass
+    
+    def __truediv__(self):
+        pass
+    
+    def __itruediv__(self):
+        pass
+    
     def conjugate(self):
         pass
     
+    def norm(self):
+        pass
