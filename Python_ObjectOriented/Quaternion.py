4175f22435c11af57c1a172ec85b4af830e7684b
Jonah Ebent <jonahaaronebent@gmail.com>
2023-11-15 17:39:29 -0500

    addition, subtraction, multiplication working

--- Python_ObjectOriented/Quaternion.py
+++ Python_ObjectOriented/Quaternion.py
@@ -1,3 +1,4 @@
+import math
 class Quaternion:
     def __init__(self, a, b, c, d) -> None:
         if any(not isinstance(i, (int, float)) for i in [a,b,c,d]):
@@ -19,39 +20,72 @@ class Quaternion:
         for num, dim in zip([self.a, self.b, self.c, self.d], ['', 'i', 'j', 'k']):
             if len(string) == 0 and num != 0:           # ensures you don't get any plus signs at the beginning of the expression
                     string += f'{str(num)}{dim}'
-            else:
+            else:                                       # note, no case for 0: don't add anything to the string
                 match signOf(num):
                     case -1.0:
                         string += f'{str(num)}{dim}'    # num provides the negative sign for subtraction
-                    case 1.0:
-                        string += f'+{str(num)}{dim}'   # add a plus sign for addition when num is positive
-                                                        # note, no case for 0: don't add anything to the string
+                    case 1.0:                           # add a plus sign for addition when num is positive
+                        string += f'+{str(num)}{dim}'                                                        
         if len(string) == 0:                            # if nothing is in the string, return 0
             return '0'
         return string
+    
     def __abs__(self):
-        pass
+        return math.sqrt(self.a**2 + self.b**2 + self.c**2 + self.d**2)
     
-    def __add__(self):
-        pass
+    def __add__(self, other):
+        if isinstance(other, (int, float)):
+            other = Quaternion(other, 0, 0, 0)
+        elif isinstance(other, complex):
+            other = Quaternion(other.real, 0, other.imag, 0)
+        elif isinstance(other, Quaternion):
+            pass
+        else:
+            raise NotImplemented
+        return Quaternion(self.a + other.a, self.b + other.b, self.c + other.c, self.d + other.d)        
     
-    def __iadd__(self):
-        pass
+    def __radd__(self, other):
+        return self + other     # works because addition is commutative
+
+    def __sub__(self, other):
+        if isinstance(other, (int, float)):
+            other = Quaternion(other, 0, 0, 0)
+        elif isinstance(other, complex):
+            other = Quaternion(other.real, 0, other.imag, 0)
+        elif isinstance(other, Quaternion):
+            pass
+        else:
+            raise NotImplemented
+        return Quaternion(self.a - other.a, self.b - other.b, self.c - other.c, self.d - other.d)
     
-    def __sub__(self):
-        pass
+    def __rsub__(self, other):
+        if isinstance(other, (int, float)):
+            other = Quaternion(other, 0, 0, 0)
+        elif isinstance(other, complex):
+            other = Quaternion(other.real, 0, other.imag, 0)
+        elif isinstance(other, Quaternion):
+            pass
+        else:
+            raise NotImplemented
+        return Quaternion(other.a - self.a, other.b - self.b, other.c - self.c, other.d - self.d)
     
-    def __isub__(self):
-        pass
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            other = Quaternion(other, 0, 0, 0)
+        elif isinstance(other, complex):
+            other = Quaternion(other.real, 0, other.imag, 0)
+        elif isinstance(other, Quaternion):
+            pass
+        else:
+            raise NotImplemented
+        a = self.a*other.a - self.b*other.b - self.c*other.c - self.d*other.d
+        b = self.a*other.b + self.b*other.a + self.c*other.d - self.d*other.c
+        c = self.a*other.c - self.b*other.d + self.c*other.a + self.d*other.b
+        d = self.a*other.d + self.b*other.c - self.c*other.b + self.d*other.a
+        return Quaternion(a,b,c,d)
     
-    def __mul__(self):
-        pass
-    
-    def __imul__(self):
-        pass
-    
-    def __rmul__(self):
-        pass
+    def __rmul__(self, other):
+        return self * other
     
     def __truediv__(self):
         pass
@@ -63,4 +97,4 @@ class Quaternion:
         pass
     
     def norm(self):
-        pass
+        return abs(self)
